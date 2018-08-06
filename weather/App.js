38e4686301a2189c635b550474d7bd36233c7f72
Pat Neel <patrick.neel@colorado.edu>
2018-08-06 13:36:51 -0600

    inital set up

---
+++ weather/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Fuck it eijfk</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
